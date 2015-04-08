a2f8331f150372d171451d8ee61e20ffb0cdc330
Joe Ebmeier <joe.ebmeier@firespring.com>
2015-04-08 15:30:19 -0500

    Adding quotes around hook dir

--- contrib/msysgit-install.cmd
+++ contrib/msysgit-install.cmd
@@ -38,9 +38,11 @@ if errorlevel 4 if not errorlevel 5 goto :AccessDenied
 if errorlevel 1 set ERR=1
 xcopy "%~dp0\..\git-flow*"           "%GIT_HOME%\bin"                 /Y /R /F || set ERR=1
 xcopy "%~dp0\..\gitflow-*"           "%GIT_HOME%\bin"                 /Y /R /F || set ERR=1
+setlocal
 set HOOK_DIR="%GIT_HOME%\local\share\doc\gitflow\hooks"
-if not exist "%HOOK_DIR%" md "%HOOK_DIR%"                             /Y /R /F || set ERR=1
-xcopy "%~dp0\..\hooks\*"             "%HOOK_DIR%"                     /Y /R /F || set ERR=1
+::if not exist "%HOOK_DIR%" md "%HOOK_DIR%"                             /Y /R /F || set ERR=1
+::xcopy "%~dp0\..\hooks\*"             "%HOOK_DIR%"                     /Y /R /F || set ERR=1
+endlocal
 
 if %ERR%==1 choice /T 30 /C Y /D Y /M "Some unexpected errors happened. Sorry, you'll have to fix them by yourself."
 
